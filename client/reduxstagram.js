cfc883771d966ff98ccbd3b56d4ad40dfcaef66e
Dennis Hedlund <denhed@Denniss-iMac.local>
2017-01-10 21:51:26 +0100

    added .babelrc and .eslintrc

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+render(<p>hi, setup is working</p>, document.getElementById('root'));
